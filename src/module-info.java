9f6ce749dc3678f664791e7b49fe59b65931e8ab
Poolynx <wesleyrab2020@gmail.com>
2024-02-13 18:25:01 -0300

    View design

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,6 @@
 module works {
 	requires javafx.controls;
-<<<<<<< HEAD
 	requires javafx.fxml;
-=======
->>>>>>> adaab7648fbb32c33f215ff69223fb5e652e58a7
-	
+
 	opens application to javafx.graphics, javafx.fxml;
 }
